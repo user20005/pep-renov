cc8af267c50f91e382ce822b2b662afeafad1516
Marzouk <nemendmd@gmail.com>
2025-12-28 14:06:57 +0100

    Updates

--- _next/static/chunks/8a7aa6f6c3bc46a1.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,71689,t=>{"use strict";let e=(0,t.i(75254).default)("arrow-left",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);t.s(["ArrowLeft",()=>e],71689)}]);
